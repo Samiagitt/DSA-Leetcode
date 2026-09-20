142a4b2968972ec19c95828bcb69a128e6bcfa79
Samia Tabassum <119676717+Samiagitt@users.noreply.github.com>
2026-09-20 07:28:27 -0700

    Time: 7 ms (42.48%), Space: 13.5 MB (12.56%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums) + 1
+        total = (n * (n-1)) // 2
+        
+        for num in nums:
+            total -= num
+        
+        return total
+        
